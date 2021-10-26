9f205a455744813bf8b09fe0025a9b83db5aa48a
jeffbcordeiro <jefferson123.0@gmail.com>
2021-10-26 17:39:16 -0300

    removido diretório desnecessário

--- src/main/java/br/com/dinheiro/irpf/aplicacao/repositorio/Pdf.java
+++
@@ -1,9 +0,0 @@
-package br.com.dinheiro.irpf.aplicacao.repositorio;
-
-import br.com.dinheiro.irpf.aplicacao.dominio.DadosPdf;
-
-import java.util.List;
-
-public interface Pdf {
-    List<DadosPdf> extraiDadosPdf(String nomeArquivo);
-}
